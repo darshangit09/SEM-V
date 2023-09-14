72f48a0e87ea1d3f57ee0c501e3a800b42c70a8c
Mr.Finaviya <100658373+darshangit09@users.noreply.github.com>
2023-09-14 14:19:54 +0530

    Update simple_insterst_cal.php

--- Web Designing/PHP/simple_insterst_cal.php
+++ Web Designing/PHP/simple_insterst_cal.php
@@ -13,17 +13,21 @@
     </form>
 
     <?php
+    function calculateSimpleInterest($loan_amount, $interest_rate, $years) {
+        $simple_interest = ($loan_amount * $interest_rate * $years) / 100;
+      
+        echo "<p>Loan Amount: $loan_amount</p>";
+        echo "<p>Rate of Interest: $interest_rate%</p>";
+        echo "<p>Years: $years</p>";
+        echo "<p>Simple Interest: $simple_interest</p>";
+      }
+      
     if(isset($_POST['calculate'])) {
         $loan_amount = $_POST['loan_amount'];
         $interest_rate = $_POST['interest_rate'];
         $years = $_POST['years'];
 
-        $simple_interest = ($loan_amount * $interest_rate * $years) / 100;
-
-        echo "<p>Loan Amount: $loan_amount</p>";
-        echo "<p>Rate of Interest: $interest_rate%</p>";
-        echo "<p>Years: $years</p>";
-        echo "<p>Simple Interest: $simple_interest</p>";
+        calculateSimpleInterest($loan_amount, $interest_rate, $years);
     }
     ?>
 </body>
